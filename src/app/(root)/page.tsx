6ad1ce860f6d8ddb05abab63833f55345cf5b959
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-05 18:42:24 +0300

    updating functionalities

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,27 +1,27 @@
 'use client';
 import { Button} from "@nextui-org/button";
-import { redirect } from "next/navigation";
+import Link from "next/link";
 export default function Home() {
 
-  const handler = () => {
-    redirect("/sign-in");
-  }
-
-  const handlerr = () => {
-    redirect("/sign-in");
-  }
+  
   return (
     <>
     <div>Landing page</div>
 
     <div className="bg-gray-400 w-[150px] h-[150%] rounded-xl border-2 border-dark-400 inline-block">
-      <Button color="primary" onClick={() => {handler}} className="w-[100%]">
+      <Button color="primary" className="w-[100%]">
+        <Link href="/sign-in">
         Sign in
+        </Link>
+        
       </Button>
 
     </div>
     <div className="bg-gray-400 w-[150px] h-[150%] rounded-xl border-2 border-dark-400 inline-block">
-      <Button color="primary" onClick={() => {handlerr}} className="w-[100%]">
+      <Button color="primary" className="w-[100%]">
+      <Link href="/sign-up">
+      Sign up
+        </Link>
         Sign up
       </Button>
 
