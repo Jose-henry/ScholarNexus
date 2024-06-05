c9afe648ce32d256117fb5d9df49b34abf8489ef
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-05 18:34:26 +0300

    updating functionalities

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Metadata } from "next";
+import { dark } from "@clerk/themes";
 
 const nunito = Nunito({ 
   subsets: ["latin"],
@@ -26,9 +27,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme: [dark],
+      variables: { colorPrimary: '#9cb9d2' },
+    }} >
     <html lang="en">
-      <body className={`${nunito.className} `}style={{ backgroundImage: `url('/assets/low-poly-grid-haikeii.svg')` }}>
+    <body className={`${nunito.className} bg-gradient-to-r from-Navbar-primary to-Navbar-secondary`}>
       <div className="w-full flex justify-center items-center min-h-screen">{children}</div>
       </body>
     </html>
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,31 @@
-
+'use client';
+import { Button} from "@nextui-org/button";
+import { redirect } from "next/navigation";
 export default function Home() {
+
+  const handler = () => {
+    redirect("/sign-in");
+  }
+
+  const handlerr = () => {
+    redirect("/sign-in");
+  }
   return (
+    <>
     <div>Landing page</div>
+
+    <div className="bg-gray-400 w-[150px] h-[150%] rounded-xl border-2 border-dark-400 inline-block">
+      <Button color="primary" onClick={() => {handler}} className="w-[100%]">
+        Sign in
+      </Button>
+
+    </div>
+    <div className="bg-gray-400 w-[150px] h-[150%] rounded-xl border-2 border-dark-400 inline-block">
+      <Button color="primary" onClick={() => {handlerr}} className="w-[100%]">
+        Sign up
+      </Button>
+
+    </div>
+    </>
   );
 }
