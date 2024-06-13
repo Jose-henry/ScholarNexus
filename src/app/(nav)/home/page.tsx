598c34d2e7269d40a174ec03c9dfe8f11eb44abd
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 14:01:57 +0100

    Changes

--- src/app/(nav)/home/page.tsx
+++ src/app/(nav)/home/page.tsx
@@ -38,23 +38,23 @@ export const metadata: Metadata = {
             <div className="h-full flex flex-col gap-[30px]">
                 <VideoCard />
                 <div className="flex gap-[10px] border-t-[1px] border-t-[#63686e] pt-3">
-                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#9a9a9a] text-[12px] shadow-lg">
+                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#545454] text-[12px] shadow-lg">
                         <Image width="16" height="16" src="/assets/all-icon.svg" alt="next"/>
                         All
                     </Button>
-                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#9a9a9a] text-[12px] shadow-lg">
+                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#545454] text-[12px] shadow-lg">
                         <Image width="16" height="16" src="/assets/articles-icon.svg" alt="next"/>
                         Articles
                     </Button>
-                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#9a9a9a] text-[12px] shadow-lg">
+                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#545454] text-[12px] shadow-lg">
                         <Image width="16" height="16" src="/assets/videos-icon.svg" alt="next"/>
                         Videos
                     </Button>
-                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#9a9a9a] text-[12px] shadow-lg">
+                    <Button variant="light" size="sm" radius="full" className="w-[90px] font-bold text-white bg-[#545454] text-[12px] shadow-lg">
                         <Image width="16" height="16" src="/assets/course-icon.svg" alt="next"/>
                         Courses
                     </Button>
-                    <Button variant="light" size="sm" radius="full" className="w-[110px] font-bold text-white bg-[#9a9a9a] text-[12px] shadow-lg">
+                    <Button variant="light" size="sm" radius="full" className="w-[110px] font-bold text-white bg-[#545454] text-[12px] shadow-lg">
                         <Image width="16" height="16" src="/assets/filled-bookmark-icon.svg" alt="next"/>
                         Bookmarked
                     </Button>
