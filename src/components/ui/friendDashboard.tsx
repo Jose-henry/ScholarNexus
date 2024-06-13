ff85619186e0ade087b56b9bebf5dbe81995a030
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 21:31:27 +0100

    Message

--- src/components/ui/friendDashboard.tsx
+++ src/components/ui/friendDashboard.tsx
@@ -3,6 +3,7 @@
 import {Avatar} from 
 "@nextui-org/avatar";
 import { Button } from "@nextui-org/button";
+import Image from "next/image";
 
 interface Props {
     firstName: string;
@@ -11,11 +12,14 @@ interface Props {
 }
 export default function FriendDashboard({firstName, lastName, friendImage}: Props) {
     return (
-        <div className="bg-gray-400 w-[100%] border-dark-400  cursor-pointer rounded-[3px] inline-block"> 
+        <div className="bg-gray-400 w-[100%] border-dark-400  cursor-pointer rounded-[3px] inline-block border border-black"> 
             <Avatar showFallback isBordered radius="full" src={friendImage} size="sm" className='cursor-pointer bg-slate-300 h-[18px] w-[18px]'> 
             </Avatar>    
             <p className="text-[12px]">{firstName + " " + lastName}</p>   
-            <Button size="lg" className="underline bg-[#393e46] text-white text-[12px]">follow</Button> 
+            <div>
+                <Button size="lg" className="underline text-[12px]">follow</Button>
+                <Image width="16" height="16" src="/assets/follow-icon.svg" alt="follow"/>
+            </div>
         </div>
     );
 }
