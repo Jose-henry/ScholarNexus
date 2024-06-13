88d94d30ba806b835255d3bd8f470acbc800fd40
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 16:29:59 +0100

    Changes

--- src/components/cards/DashboardFriends.tsx
+++ src/components/cards/DashboardFriends.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 export default function DashboardFriends() {
     return (
         <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-2">
-            <div>
+            <div className="flex gap-1 items-center">
                 <h2 className="text-[12.5px] text-[#070f4e] font-bold">Friend Suggestions</h2>
-                <Image width="16" height="16" src="/assets/deselect-icon.svg" alt="deselect"/>
+                <Image width="16" height="16" src="/assets/friends-icon.svg" alt="deselect"/>
             </div>
             <div className="w-full h-[90%] border border-black">
                 
--- src/components/cards/DashboardTask.tsx
+++ src/components/cards/DashboardTask.tsx
@@ -6,7 +6,7 @@ export default function DashboardTask() {
     return (
         <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-2"> 
             <div className="flex gap-1 items-center">
-                <h2 className="text-[12.5px] text-[#070f4e] font-bold">Friend Suggestions</h2>
+                <h2 className="text-[12.5px] text-[#070f4e] font-bold">Tasks</h2>
                 <Image width="16" height="16" src="/assets/task-icon.svg" alt="deselect"/>
             </div>
             <div className="w-full h-[90%] border border-black">
