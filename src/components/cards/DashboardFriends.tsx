a7e5182214e5c4691955117529236fa3ecb7c4c4
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 16:38:14 +0100

    Messages

--- src/components/cards/DashboardFriends.tsx
+++ src/components/cards/DashboardFriends.tsx
@@ -6,7 +6,7 @@ export default function DashboardFriends() {
     return (
         <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-2">
             <div className="flex gap-1 items-center">
-                <h2 className="text-[12.5px] text-[#070f4e] font-black">Friend Suggestions</h2>
+                <h2 className="text-[12.5px] text-[#070f4e] font-extrabold">Friend Suggestions</h2>
                 <Image width="18" height="18" src="/assets/friends-icon.svg" alt="deselect"/>
             </div>
             <div className="w-full h-[90%] border border-black">
