8da8ea828b498e03a7720836f19bf87277a8fd09
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 22:32:23 +0100

    Message

--- src/components/cards/DashboardFriends.tsx
+++ src/components/cards/DashboardFriends.tsx
@@ -7,8 +7,11 @@ export default function DashboardFriends() {
     return (
         <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-3 flex flex-col justify-between">
             <div className="flex gap-1 items-center">
-                <h2 className="text-[12.5px] text-[#070f4e] font-extrabold">Friend Suggestions</h2>
-                <Image width="18" height="18" src="/assets/friends-icon.svg" alt="deselect"/>
+                <div className="flex gap-1 justify-between">
+                    <Image width="18" height="18" src="/assets/follow-icon.svg" alt="friend"/>
+                    <h2 className="text-[12.5px] text-[#070f4e] font-extrabold">Friend Suggestions</h2>
+                </div>
+                
             </div>
             <div className="w-full h-[85%] bg-[#f2f2f2] shadow-md shadow-[#50595c] flex flex-col justify-between p-2">
                 <FriendDashboard firstName="John" lastName="Doe" level="100" friendImage="/assets/friend1.png"/>
