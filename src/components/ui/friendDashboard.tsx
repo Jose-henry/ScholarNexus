b9aa055771a1d1554f5f92657921ea097530489f
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-13 20:44:55 +0100

    new

--- src/components/ui/friendDashboard.tsx
+++ src/components/ui/friendDashboard.tsx
@@ -2,13 +2,20 @@
 
 import {Avatar} from 
 "@nextui-org/avatar";
+import { Button } from "@nextui-org/button";
 
-
-export default function FriendDashboard() {
+interface Props {
+    firstName: string;
+    lastName: string;
+    friendImage: string;
+}
+export default function FriendDashboard({firstName, lastName, friendImage}: Props) {
     return (
-        <div className="bg-gray-400 w-[100%] h-[250px] border-dark-400  cursor-pointer rounded-[3px] inline-block"> 
-            <Avatar showFallback isBordered radius="full" src="(link unavailable)" size="sm" className='cursor-pointer bg-slate-300 h-[25px] w-[25px]'> 
-            </Avatar>        
+        <div className="bg-gray-400 w-[80%] h-[250px] border-dark-400  cursor-pointer rounded-[3px] inline-block"> 
+            <Avatar showFallback isBordered radius="full" src={friendImage} size="sm" className='cursor-pointer bg-slate-300 h-[25px] w-[25px]'> 
+            </Avatar>    
+            <p>{firstName + " " + lastName}</p>   
+            <Button size="sm" className="underline bg-[#393e46] text-white">follow</Button> 
             
             </div>
     );
