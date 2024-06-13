55d1c22a968307d3f282a3af480ec1ec715ede03
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 14:35:36 +0100

    Change

--- src/components/cards/UserWelcomeCard.tsx
+++ src/components/cards/UserWelcomeCard.tsx
@@ -6,7 +6,7 @@ export default function UserWelcomeCard() {
     return (
         <div className="flex flex-col items-center gap-1">
             <Avatar isBordered showFallback color="secondary" className="w-20 h-20 text-large mb-[10px] cursor-pointer" src="(link unavailable)" />
-            <h3 className="text-[#eef2e2] font-[1000] text-[21px]">Welcome Ella!</h3>
+            <h3 className="text-[#eef2e2] font-[1000] text-[25px]">Welcome Ella!</h3>
             <p className="text-white text-[14px] mt-[-3px]">Keep the learning going!</p>
         </div>
     );
