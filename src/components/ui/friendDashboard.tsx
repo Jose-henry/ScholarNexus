664bb006b7e8fc3fa9bca3ec4409fc3e4f63322b
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 22:07:18 +0100

    Message

--- src/components/ui/friendDashboard.tsx
+++ src/components/ui/friendDashboard.tsx
@@ -14,19 +14,16 @@ interface Props {
 
 export default function FriendDashboard({firstName, lastName, level, friendImage}: Props) {
     return (
-        <div className="bg-gray-400 w-[100%] cursor-pointer rounded-[3px] flex justify-between p-1"> 
+        <div className="bg-gray-400 w-[100%] cursor-pointer rounded-[3px] flex justify-between p-1 pl-2 pr-2"> 
             <div className="flex gap-2 items-center">
-                <Avatar showFallback isBordered radius="full" src={friendImage} size="sm" className='cursor-pointer bg-slate-300 h-[20px] w-[20px]'> 
+                <Avatar showFallback isBordered radius="full" src={friendImage} size="sm" className='cursor-pointer bg-slate-300 h-[22px] w-[22px]'> 
                 </Avatar>    
                 <div>
                     <p className="text-[11px] font-bold">{firstName + " " + lastName}</p>
                     <p className="text-[10px]">{level} level</p>
                 </div> 
             </div>
-            <div className="flex gap-1 items-center">
-                
-                <Image width="15" height="15" src="/assets/follow-icon.svg" alt="follow"/>
-            </div>
+            <Button size="lg" className="underline text-[10px] text-[#070f4e] font-semibold hover:no-underline">follow</Button>
         </div>
     );
 }
