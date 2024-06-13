691122660eae5313f2faaaa219eacd4df24f3cee
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-13 20:36:50 +0100

    new

---
+++ src/components/ui/friendDashboard.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {Avatar} from 
+"@nextui-org/avatar";
+
+
+export default function FriendDashboard() {
+    return (
+        <div className="bg-gray-400 w-[100%] h-[250px] border-dark-400  cursor-pointer rounded-[3px] inline-block"> 
+            <Avatar showFallback isBordered radius="full" src="(link unavailable)" size="sm" className='cursor-pointer bg-slate-300 h-[25px] w-[25px]'> 
+            </Avatar>        
+            
+            </div>
+    );
+}
