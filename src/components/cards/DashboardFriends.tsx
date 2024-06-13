d78f7dc2fb3ce7e01bda07c0d31fe9e59b39e1ff
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 16:50:57 +0100

    Messages

--- src/components/cards/DashboardFriends.tsx
+++ src/components/cards/DashboardFriends.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 
 export default function DashboardFriends() {
     return (
-        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-2 flex flex-col justify-between">
+        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-3 flex flex-col justify-between">
             <div className="flex gap-1 items-center">
                 <h2 className="text-[12.5px] text-[#070f4e] font-extrabold">Friend Suggestions</h2>
                 <Image width="18" height="18" src="/assets/friends-icon.svg" alt="deselect"/>
             </div>
-            <div className="w-full h-[88%] bg-[#f2f2f2] rounded-sm shadow-md">
+            <div className="w-full h-[88%] bg-[#f2f2f2] shadow-md">
                 
             </div>
         </div>
--- src/components/cards/DashboardTask.tsx
+++ src/components/cards/DashboardTask.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 
 export default function DashboardTask() {
     return (
-        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-2 flex flex-col justify-between"> 
+        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-3 flex flex-col justify-between"> 
             <div className="flex gap-1 items-center">
                 <h2 className="text-[12.5px] text-[#070f4e] font-extrabold">Tasks</h2>
                 <Image width="16" height="16" src="/assets/task-icon.svg" alt="deselect"/>
             </div>
-            <div className="w-full h-[88%] bg-[#f2f2f2] rounded-sm shadow-md">
+            <div className="w-full h-[88%] bg-[#f2f2f2] shadow-md">
 
             </div>
         </div>
