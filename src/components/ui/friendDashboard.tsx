2bb20c79f899afe5d3ae85de2442d2e45ded992a
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 21:16:53 +0100

    Message

--- src/components/ui/friendDashboard.tsx
+++ src/components/ui/friendDashboard.tsx
@@ -11,12 +11,11 @@ interface Props {
 }
 export default function FriendDashboard({firstName, lastName, friendImage}: Props) {
     return (
-        <div className="bg-gray-400 w-[80%] h-[250px] border-dark-400  cursor-pointer rounded-[3px] inline-block"> 
+        <div className="bg-gray-400 w-[100%] border-dark-400  cursor-pointer rounded-[3px] inline-block"> 
             <Avatar showFallback isBordered radius="full" src={friendImage} size="sm" className='cursor-pointer bg-slate-300 h-[25px] w-[25px]'> 
             </Avatar>    
             <p>{firstName + " " + lastName}</p>   
             <Button size="sm" className="underline bg-[#393e46] text-white">follow</Button> 
-            
-            </div>
+        </div>
     );
 }
