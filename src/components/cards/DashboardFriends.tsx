8a13772a74982f011cb848a95bce25ecdc866361
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 15:56:41 +0100

    Changes

--- src/components/cards/DashboardFriends.tsx
+++ src/components/cards/DashboardFriends.tsx
@@ -2,6 +2,11 @@
 
 export default function DashboardFriends() {
     return (
-        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black"> </div>
+        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black">
+            <h2>Friend Suggestions</h2>
+            <div>
+                
+            </div>
+        </div>
     );
 }
