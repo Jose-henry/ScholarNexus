a4f6842cfb9d6bf639cdfbb92e16b8f4598544a5
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-13 15:59:26 +0100

    Messages

--- src/components/cards/DashboardFriends.tsx
+++ src/components/cards/DashboardFriends.tsx
@@ -2,7 +2,7 @@
 
 export default function DashboardFriends() {
     return (
-        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black">
+        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-2">
             <h2 className="text-sm">Friend Suggestions</h2>
             <div>
                 
--- src/components/cards/DashboardTask.tsx
+++ src/components/cards/DashboardTask.tsx
@@ -2,7 +2,7 @@
 
 export default function DashboardTask() {
     return (
-        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black"> 
+        <div className="w-[100%] h-[200px] bg-[#fbfbfb] rounded-[2px] cursor-pointer shadow-md shadow-black p-2"> 
             <h2 className="text-sm">Tasks</h2>
             <div>
 
