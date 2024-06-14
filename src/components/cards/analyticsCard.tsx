45bcb24352ec87af84a66f1552fb8ad24d4c64d3
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-14 19:41:42 +0100

    Messages

---
+++ src/components/cards/analyticsCard.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function analyticsCard() {
+    return (
+        <div className="w-[100%] h-["> 
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
