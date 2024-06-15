25dad30022616784b300255c8004bc38b61991ac
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-15 01:30:43 +0100

    Messages

--- src/components/ui/taskDashboard.tsx
+++ src/components/ui/taskDashboard.tsx
@@ -7,13 +7,17 @@ import Image from "next/image";
 
 interface Props {
     task: string;
+    date: any;
 }
 
-export default function TaskDashboard({task}: Props) {
+export default function TaskDashboard({task, date}: Props) {
     return (
-        <div className=""> 
-            <Image width="20" height="20" src="/assets/completed.svg" alt="tikcet"/>
-            <p>{task}</p>
+        <div className="flex bg-white border-b-[1px] border-b-[#dee1ec] p-1 pr-2 shadow-sm pl-3 justify-between items-center"> 
+            <div className="flex gap-2">
+                <Image width="12" height="12" src="/assets/circle.svg" alt="tikcet"/>
+                <p className="text-[11px] font-semibold">{task}</p>
+            </div>
+            <p className="text-[9px] font-bold text-[#152744]">{date}</p>
         </div>
     );
 }
