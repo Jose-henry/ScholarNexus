47eaed2df12d902491d62718a083aef2cd30d9b7
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-16 19:39:06 +0100

    new changes

--- src/lib/actions/coursera.actions.ts
+++ src/lib/actions/coursera.actions.ts
@@ -1,2 +1,12 @@
 const apiKey = 'AuyVgcBcZxYDmxPkXPfL2OAZId0TuVWDmWjoiX3LqkUlt19o'
 const secret = '2Y9ooXo5HGO6llJxaqewssHOIz2iuHdvQ5JMnoh2AjPIl13P042iLQTUtTzNz8ib';
+
+
+
+// {
+//     "token_type": "Bearer",
+//     "access_token": "zG1AZ8oymRjbNsnZ31UTQvhprj6n",
+//     "grant_type": "client_credentials",
+//     "issued_at": 1718562646,
+//     "expires_in": 1799
+//   }
