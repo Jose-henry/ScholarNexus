15c1e63410ef3c3eacb506a1d482d6587fbc8640
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-16 21:32:50 +0100

    new changes

---
+++ src/lib/actions/book.action.ts
@@ -0,0 +1,20 @@
+
+
+
+
+export default async function getBooks(userInfo: any) {
+    const interests = userInfo?.interests; // assuming interests is an array of strings
+ // replace with your actual API key
+    const query = interests?.join(' OR '); // join interests with ' OR ' for the search query
+
+    const url = `https://openlibrary.org/search.json?q=${query}`;
+    try {   
+      const response = await fetch(url);
+      const data = await response.json()
+      return data.items; // return the articles array
+    } catch (error) {
+      console.error(error);
+      // Handle the error here, e.g. by displaying an error message to the users
+      
+    }
+  }
