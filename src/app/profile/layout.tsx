4a1ccb0b41486c672e947f2ce57bde1e3392fe0b
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-16 10:51:12 +0100

    new

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,11 +1,17 @@
-import type { Metadata } from "next";
+
 import { Nunito } from "next/font/google";
 import "../globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider
+} from '@clerk/nextjs'
+import NavBar from "@/components/shared/NavBar";
+import TopBar from "@/components/shared/topbar";
+import BottomBar from "@/components/shared/bottombar";
+import { Metadata } from "next";
 
 const nunito = Nunito({ 
   subsets: ["latin"],
-  weight: ["200", "300", "400", "500", "600", "700"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
   variable: '--font-nunito' 
 });
 
@@ -17,6 +23,8 @@ export const metadata: Metadata = {
   description: "An Educational Productivity tool",
 };
 
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,10 +33,24 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={nunito.className}>
-       {children}
+      <body
+      className={nunito.className}
+      style={{
+        height: "100vh"
+      }}>
+        <div id="wrapper" className="h-[100%] flex sm:p-0  gap-[1%] bg-[#fafafa]">
+            <NavBar />
+            <div id="container" className="w-[100%] flex flex-col h-[100%]">
+              <TopBar />
+              <main className="flex-1 ml-[12px] ">
+                  {children}
+              </main>
+                <BottomBar />
+            </div>
+          </div>
       </body>
     </html>
+
     </ClerkProvider>
   );
 }
--- src/app/profile/loading.tsx
+++ src/app/profile/loading.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import React from 'react';
+import CustomizedProgressBars from '../(nav)/loadingComponent';
+
 
 export default function Loading() {
-    return (
-        <div>Loading...</div>
-    );
+  return (
+    <div>
+      <CustomizedProgressBars />
+    </div>
+  );
 }
