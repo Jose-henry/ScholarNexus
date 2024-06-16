72a185aba208a33cf221d6cf89b9b16d2fe138c7
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-16 20:57:14 +0100

    new changes

--- src/lib/actions/youtube.action.ts
+++ src/lib/actions/youtube.action.ts
@@ -1,5 +1,27 @@
 
 
+"use server"
+
+export default async function getYoutube(userInfo: any) {
+    const interests = userInfo?.interests; // assuming interests is an array of strings
+    const apiKey = 'AIzaSyBVKd-rLT1bRD42RJaExTdK5emHyqcgHNg';
+ // replace with your actual API key
+    const query = interests?.join(' OR '); // join interests with ' OR ' for the search query
+    const today = new Date();
+    const oneWeekAgo = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000);
+  const fromDate = oneWeekAgo.toISOString().split('T')[0];
 
 
-const apikey = 'AIzaSyBVKd-rLT1bRD42RJaExTdK5emHyqcgHNg';
+    const url = `https://newsapi.org/v2/everything?q=${query}&from=${fromDate}&sortBy=relevancy&pageSize=9&apiKey=${apiKey}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data.articles; // return the articles array
+    } catch (error) {
+      console.error(error);
+      // Handle the error here, e.g. by displaying an error message to the users
+      
+    }
+  };
+
+  
