b59ad6f302230e6a5789b7be974acea3b24133d0
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-16 01:28:20 +0100

    new and improved changes

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,18 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
-
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-} else {
-    // @ts-ignore
-    if (!global.prisma) {
-        // @ts-ignore
-        global.prisma = new PrismaClient();
-    }
-    // @ts-ignore
-    prisma = global.prisma;
-}
-
-export default prisma
+export default prisma;
