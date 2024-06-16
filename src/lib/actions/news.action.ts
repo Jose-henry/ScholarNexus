3081ae957f8a8c63b78d7f04df15e64f84d4d1e5
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-16 17:14:32 +0100

    new changes

--- src/lib/actions/news.action.ts
+++ src/lib/actions/news.action.ts
@@ -1,15 +1,18 @@
 "use server"
 
 export default async function getNews(userInfo:any) {
+
     const interests = userInfo?.interests; // assuming interests is an array of strings
-    const apiKey = process.env.GOOGLE_NEWS_API_KEY;
+    const apiKey = '3770aa9b63bdf22dad53c34c337114ce';
     const query = interests?.join(' OR '); // join interests with ' OR ' for the search query
+
+    const encodedQuery = encodeURIComponent(query);
 
     const fromDate = new Date(new Date().getTime() - 365 * 24 * 60 * 60 * 1000).toISOString().split('T')[0] + 'T00:00:00Z';
     const toDate = new Date().toISOString();
 
-    const url = `https://gnews.io/api/v4/search?q= ` + query + '&lang=en&max=9&apikey=' + apiKey + '&sortby=relevance&from=' + fromDate + '&to=' + toDate;
-
+    // Include the encoded query in the URL
+    const url = `https://gnews.io/api/v4/search?q=${encodedQuery}&lang=en&max=9&apikey=${apiKey}&sortby=relevance&from=${fromDate}&to=${toDate}`;
     try {
         const response = await fetch(url);
         const data = await response.json();
