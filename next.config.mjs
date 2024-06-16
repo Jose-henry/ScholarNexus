0de925b0290293e7840834ab2cd3c5dfb1ead754
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-16 01:45:29 +0100

    new and improved changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
   images: {
     remotePatterns: [
       {
