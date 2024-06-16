29ca27b18a24f50c438e6c3f32d174c321d71a70
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-16 10:01:55 +0100

    new

--- src/components/forms/homeTask.tsx
+++ src/components/forms/homeTask.tsx
@@ -43,10 +43,7 @@ interface TaskMenuProps {
     label: string
   }
 
-export function HomeTaskMenu({task_title}: TaskMenuProps) {    
-  const [label, setLabel] = React.useState("label")
-  const [open, setOpen] = React.useState(false)
-
+export function HomeTaskMenu({task_title, label}: TaskMenuProps) {    
   return (
     <div className="flex w-full flex-col items-start justify-between px-3 py-2 sm:flex-row sm:items-center bg-white border-b-[1px] border-b-[#929aab]">
       <div className="flex gap-4 items-center w-[90%]">
@@ -55,8 +52,11 @@ export function HomeTaskMenu({task_title}: TaskMenuProps) {
           <span className="mr-2 rounded-full w-[80px] px-2 py-1 text-xs text-primary-foreground bg-[#142d4c]">
             {label}
           </span>
-          <input className="p-2 outline-none bg-white w-[80%] text-[13px]" placeholder="Enter task..."></input>
+          <p className="p-2 outline-none bg-white w-[80%] text-[13px]">{task_title}</p>
         </div>
+      </div>
+      <div>
+        
       </div>
     </div>
   )
