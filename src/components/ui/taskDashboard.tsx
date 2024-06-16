2968bd3ce50a33362dbe01998711cddef5579907
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-16 10:26:45 +0100

    new

--- src/components/ui/taskDashboard.tsx
+++ src/components/ui/taskDashboard.tsx
@@ -8,20 +8,14 @@ import ToDoBtn from "../forms/toDoBtn";
 
 interface Props {
     task: string;
-    label: string;
     date: any;
 }
 
-export default function TaskDashboard({task, date, label}: Props) {
+export default function TaskDashboard({task, date}: Props) {
     return (
         <div className="flex bg-white border-b-[1px] border-b-[#dee1ec] p-1 pr-2 shadow-sm pl-3 justify-between items-center "> 
             <div className="flex gap-2">
-                <div className="flex gap-4">
-                    <ToDoBtn />
-                    <span className="mr-2 rounded-full w-[80px] px-2 py-1 text-xs text-primary-foreground bg-[#142d4c]">
-                        {label}
-                    </span>
-                </div>
+                <ToDoBtn />
                 <p className="text-[11px] font-semibold">{task}</p>
             </div>
             <p className="text-[9px] font-bold text-[#152744]">{date}</p>
