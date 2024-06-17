6772ec7c7548fcd49d9c7d2d4fde98a92b729f4c
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-17 18:39:09 +0100

    new

--- src/components/cards/ThreadArticleCard.tsx
+++ src/components/cards/ThreadArticleCard.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,38 +9,35 @@ interface Props {
     link: string;
     title: string;
     description: string;
-    loader: any
+    loader: any;
 };
 
 export default function ThreadCard1({ imageUrl, w, h, link, title, description, loader }: Props) {
     return (
-        <div className="bg-white shadow-md shadow-slate-600 p-[12px] rounded-sm grid gap-[10px] justify-between hover:shadow-lg hover:shadow-slate-700 transition duration-200 " style={{ gridTemplateColumns: "1fr"}}>
+        <div className="bg-white shadow-md shadow-slate-600 p-[12px] rounded-sm grid gap-[10px] justify-between hover:shadow-lg hover:shadow-slate-700 transition duration-200" style={{ gridTemplateColumns: "1fr"}}>
             <div className="h-[150px] cursor-pointer">
-            <Image 
-                src={imageUrl} 
-                alt=""  
-                width={w} 
-                height={h}  
-                className="h-full w-full object-cover rounded-sm"
-                layout="responsive"
-                loader={loader}
-                unoptimized// And this line
-                
-            />
+                <Image 
+                    src={imageUrl} 
+                    alt=""  
+                    width={w} 
+                    height={h}  
+                    className="h-full w-full object-cover rounded-sm"
+                    layout="responsive"
+                    loader={loader}
+                    unoptimized
+                />
             </div>
 
-
-            <div className="text-neutral-600 hover:translate-y-2 transition duration-200 h-[50px] border border-black overflow-ellipsis">
-            <h2 className="font-extrabold text-[#142d4c] text-[14px]">{title}</h2>
-            <p className="text-left text-[12px] text-black">{description}</p>
+            <div className="text-neutral-600 hover:translate-y-2 transition duration-200 h-[50px] overflow-hidden">
+                <h2 className="font-extrabold text-[#142d4c] text-[14px] truncate">{title}</h2>
+                <p className="text-left text-[12px] text-black overflow-hidden overflow-ellipsis whitespace-nowrap">{description}</p>
             </div>
             <div className="flex gap-1 justify-end">
-            <Image src="/assets/filled-bookmark-icon.svg" alt="" width={18} height={18} className=" cursor-pointer"></Image>
-            <Link href={link} className=" cursor-pointer">
-            <Image src="/assets/external-link.svg" alt="" width={18} height={18}></Image>
-            </Link>
+                <Image src="/assets/filled-bookmark-icon.svg" alt="" width={18} height={18} className="cursor-pointer"></Image>
+                <Link href={link} className="cursor-pointer">
+                    <Image src="/assets/external-link.svg" alt="" width={18} height={18}></Image>
+                </Link>
             </div>
-
         </div>
     );
 }
