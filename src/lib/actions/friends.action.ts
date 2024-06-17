262c9910532f02fa569444d251a95be2c5fb9929
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-17 09:10:53 +0100

    new changes

---
+++ src/lib/actions/friends.action.ts
@@ -0,0 +1,25 @@
+
+// lib/actions/friend.action.ts
+
+import prisma from "@/utils/connect";
+interface FriendParams {
+  userId: string;
+  username: string;
+  email: string;
+  accepted?: boolean;
+  rejected?: boolean;
+  pending?: boolean;
+  blocked?: boolean;
+}
+
+export async function upsertFriend({
+  userId,
+  username,
+  email,
+  accepted = false,
+  rejected = false,
+  pending = true,
+  blocked = false,
+}: FriendParams): Promise<void> {
+    
+}
