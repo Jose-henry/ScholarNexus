3a2331577ca394a65a894a8f9323970d8ab57774
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 03:43:04 +0100

    new

--- src/app/(nav)/tasks/page.tsx
+++ src/app/(nav)/tasks/page.tsx
@@ -7,23 +7,21 @@ import AnalyticsCard from "@/components/cards/analyticsCard";
 import Image from "next/image";
 import { CompletedDropdownMenu } from "@/components/forms/completed";
 
-
-
 export const metadata: Metadata = {
-  title:"ToDo" };
-
-
+  title: "ToDo"
+};
 
 export default async function Task() {
-  const User = await currentUser()
-    if (!User) return null; // to avoid typescript warnings
-    const userInfo = await getUserByClerkId(User?.id)
-    if (userInfo?.onboarded === false) {
-      redirect("/onboarding")};
+  const User = await currentUser();
+  if (!User) return null; // to avoid typescript warnings
+  const userInfo = await getUserByClerkId(User?.id);
+  if (userInfo?.onboarded === false) {
+    redirect("/onboarding");
+  }
 
   return (
     <div className="h-full flex flex-col gap-7 pr-5 pt-5">
-      <AnalyticsCard/>
+      <AnalyticsCard />
       <div className="flex flex-col gap-4">
         <div className="flex items-center gap-2 border-t-[1px] border-t-black pt-2">
           <button className="text-[12px] font-bold bg-[#132743] text-white px-2 py-1 shadow-md rounded-full flex items-center gap-1 hover:bg-[#497285] active:bg-[#142d4c]">
@@ -50,7 +48,7 @@ export default async function Task() {
           </div>
           <div className="rounded-sm p-2 flex flex-col gap-2 h-[365px]">
             <h2 className="text-[13px] font-semibold">Completed</h2>
-            <div className=" overflow-y-scroll h-full">
+            <div className="overflow-y-scroll h-full">
               <CompletedDropdownMenu title="Your completed tasks will appear here." />
               <CompletedDropdownMenu title="e.g see professor and submit paperwork." />
             </div>
