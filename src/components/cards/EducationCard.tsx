0588d8ee44fea13815059fd19afd9636a3b60712
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 18:16:50 +0100

    changes

--- src/components/cards/EducationCard.tsx
+++ src/components/cards/EducationCard.tsx
@@ -12,8 +12,15 @@ interface Props {
 
 export default function EducationCard({programme, school, level}: Props) {
     return (
-        <div> 
-            
+        <div className="rounded-sm bg-white shadow-md p-4 flex flex-col gap-2"> 
+            <h2 className="font-bold text-[15px]">Education</h2>
+            <div>
+                <Image src="/assests/edu-pic.svg" width={130} height={130} alt="education"></Image>
+                <div>
+                    <h2>School: {school}</h2>
+                    <h2>Programme: {programme}</h2>
+                </div>
+            </div>
         </div>
     );
 }
