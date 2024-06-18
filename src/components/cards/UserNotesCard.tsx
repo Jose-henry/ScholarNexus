e2de88ef684ca381a94f2dc2cc5fb5853e1f7b38
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 21:22:53 +0100

    changes

--- src/components/cards/UserNotesCard.tsx
+++ src/components/cards/UserNotesCard.tsx
@@ -13,26 +13,38 @@ export default function UserNotesCard() {
                 <div className="">
 
                 </div>
-                <div className="bg-[#dee1ec] p-2 rounded-sm  h-[280px] overflow-y-scroll flex flex-wrap gap-3 items-start">
+                <div className="bg-[#dee1ec] p-2 rounded-sm  h-[280px] overflow-y-scroll gap-x-3 items-start">
                     <div className="w-[100px] flex flex-col items-center">
                         <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
                         <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
                     </div>
-                    <div className="w-50 flex flex-col items-center">
+                    <div className="w-[100px] flex flex-col items-center">
                         <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                        <h2 className="text-[11px] font-semibold text-center">Default folder</h2>
+                        <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
                     </div>
-                    <div className="w-50 flex flex-col items-center">
+                    <div className="w-[100px] flex flex-col items-center">
                         <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                        <h2 className="text-[11px] font-semibold text-center">Default folder</h2>
+                        <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
                     </div>
-                    <div className="w-50 flex flex-col items-center">
+                    <div className="w-[100px] flex flex-col items-center">
                         <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                        <h2 className="text-[11px] font-semibold text-center">Default folder</h2>
+                        <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
                     </div>
-                    <div className="w-50 flex flex-col items-center">
+                    <div className="w-[100px] flex flex-col items-center">
                         <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                        <h2 className="text-[11px] font-semibold text-center">Default folder</h2>
+                        <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
+                    </div>
+                    <div className="w-[100px] flex flex-col items-center">
+                        <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
+                        <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
+                    </div>
+                    <div className="w-[100px] flex flex-col items-center">
+                        <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
+                        <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
+                    </div>
+                    <div className="w-[100px] flex flex-col items-center">
+                        <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
+                        <h2 className="text-[11px] font-semibold text-center w-[90%]">Default folder</h2>
                     </div>
                 </div>
             </div>
