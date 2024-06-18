5165791c6c8e5d2b975531032db2c6ca752a5a4e
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 23:43:52 +0100

    changes

--- src/components/cards/UserNotesCard.tsx
+++ src/components/cards/UserNotesCard.tsx
@@ -4,9 +4,9 @@ export default function UserNotesCard() {
     return (
         <div className="rounded-sm bg-white shadow-sm border-[1.5px] border-[#d3d6db] p-3">
             <h2 className="text-[14px] font-bold">Notes</h2>
-            <div className="bg-[#dee1ec] p-2 rounded-sm h-[280px] overflow-y-scroll grid grid-cols-6 gap-3 grid-auto-rows-[80px]">
-                {["Biology", "Chemistry", "Physics", "Mathematics", "Geography", "History", "Literature", "Philosophy", "Art", "Music"].map(subject => (
-                    <div key={subject} className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
+            <div className="bg-[#dee1ec] p-2 rounded-sm h-[280px] overflow-y-scroll grid grid-cols-6 gap-3">
+                {["Biology", "Chemistry", "Physics", "Mathematics", "Geography", "History", "Literature", "Philosophy", "Art", "Music", "Philisophy", "Science", "Literature", "Philosophy", "Art", "Music", , "Literature", "Philosophy", "Art", "Music", , "Literature", "Philosophy", "Art", "Music"].map(subject => (
+                    <div key={subject} className="w-[80px] h-[80px] flex flex-col p-2 items-center bg-white rounded-sm shadow-md">
                         <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
                         <h2 className="text-[11px] font-semibold text-center">{subject}</h2>
                     </div>
