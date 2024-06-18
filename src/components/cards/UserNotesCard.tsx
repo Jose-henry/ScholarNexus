a8a08355de8c66f9cd1a3a5bc7cee217bcfd0ab1
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 22:56:52 +0100

    changes

--- src/components/cards/UserNotesCard.tsx
+++ src/components/cards/UserNotesCard.tsx
@@ -6,9 +6,9 @@ export default function UserNotesCard() {
             <h2 className="text-[14px] font-bold">Notes</h2>
             <div className="bg-[#dee1ec] p-2 rounded-sm h-[280px] overflow-y-scroll flex flex-wrap gap-4">
                 {/* Use flex-wrap and gap to manage wrapping */}
-                <div className="w-[80px] h-[70px] flex flex-col items-center border border-black overflow-hidden">
+                <div className="w-[80px] min-h-[70px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1 overflow-ellipsis whitespace-nowrap">Default foldergfuftutdfy</h2>
+                    <h2 className="text-[11px] font-semibold text-center truncate-text">Anatomy and Socioalogy</h2>
                 </div>
                 <div className="w-[80px] flex flex-col items-center">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
