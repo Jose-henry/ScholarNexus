f337ccf008d42834471bcce5ccc876b55daf966d
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 23:08:15 +0100

    changes

--- src/components/cards/EducationCard.tsx
+++ src/components/cards/EducationCard.tsx
@@ -17,9 +17,9 @@ export default function EducationCard({programme, school, level}: Props) {
             <div className="flex items-center gap-4">
                 <Image src="/assets/edu-pic.svg" width={150} height={150} alt="education"></Image>
                 <div className="flex flex-col gap-2">
-                    <h2 className="text-[#142d4c] font-extrabold text-[20px]">School: <span className="font-semibold text-black">{school}</span></h2>
-                    <h2 className="text-[#142d4c] font-extrabold text-[20px]">Programme: <span className="font-semibold text-black">{programme}</span></h2>
-                    <h2 className="text-[#142d4c] font-extrabold text-[20px]">Level: <span className="font-semibold text-black">{level}</span></h2>
+                    <h2 className="text-[#142d4c] font-extrabold text-[18px]">School: <span className="font-semibold text-black">{school}</span></h2>
+                    <h2 className="text-[#142d4c] font-extrabold text-[18px]">Programme: <span className="font-semibold text-black">{programme}</span></h2>
+                    <h2 className="text-[#142d4c] font-extrabold text-[18px]">Level: <span className="font-semibold text-black">{level}</span></h2>
                 </div>
             </div>
         </div>
--- src/components/cards/UserNotesCard.tsx
+++ src/components/cards/UserNotesCard.tsx
@@ -4,39 +4,47 @@ export default function UserNotesCard() {
     return (
         <div className="rounded-sm bg-white shadow-sm border-[1.5px] border-[#d3d6db] p-3">
             <h2 className="text-[14px] font-bold">Notes</h2>
-            <div className="bg-[#dee1ec] p-2 rounded-sm h-[280px] overflow-y-scroll flex flex-wrap gap-4">
+            <div className="bg-[#dee1ec] p-2 rounded-sm h-[280px] overflow-y-scroll grid grid-cols-6 gap-3">
                 {/* Use flex-wrap and gap to manage wrapping */}
-                <div className="w-[80px] min-h-[70px] flex flex-col p-2 items-center border border-black">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center truncate-text">Anatomy and Socioalogy</h2>
+                    <h2 className="text-[11px] font-semibold text-center">Biology</h2>
                 </div>
-                <div className="w-[80px] flex flex-col items-center">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1">Default folder</h2>
+                    <h2 className="text-[11px] font-semibold text-center">Chemistry</h2>
                 </div>
-                <div className="w-[80px] flex flex-col items-center">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1">Default folder</h2>
+                    <h2 className="text-[11px] font-semibold text-center">Physics</h2>
                 </div>
-                <div className="w-[80px] flex flex-col items-center">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1">Default folder</h2>
+                    <h2 className="text-[11px] font-semibold text-center">Mathematics</h2>
                 </div>
-                <div className="w-[80px] flex flex-col items-center">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1">Default folder</h2>
+                    <h2 className="text-[11px] font-semibold text-center">Geography</h2>
                 </div>
-                <div className="w-[80px] flex flex-col items-center">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1">Default folder</h2>
+                    <h2 className="text-[11px] font-semibold text-center">History</h2>
                 </div>
-                <div className="w-[80px] flex flex-col items-center">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1">Default folder</h2>
+                    <h2 className="text-[11px] font-semibold text-center">Literature</h2>
                 </div>
-                <div className="w-[80px] flex flex-col items-center">
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
                     <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
-                    <h2 className="text-[11px] font-semibold text-center mt-1">Default folder</h2>
+                    <h2 className="text-[11px] font-semibold text-center">Philosophy</h2>
+                </div>
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
+                    <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
+                    <h2 className="text-[11px] font-semibold text-center">Art</h2>
+                </div>
+                <div className="w-[80px] h-[80px] flex flex-col p-2 items-center border border-black">
+                    <Image src="/assets/folder-icon.svg" alt="folder" width={50} height={50} />
+                    <h2 className="text-[11px] font-semibold text-center">Music</h2>
                 </div>
             </div>
         </div>
