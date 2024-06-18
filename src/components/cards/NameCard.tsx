974ed4aed978cee51e289a7c1bbbada1d2b8c0b8
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 15:48:57 +0100

    changes

--- src/components/cards/NameCard.tsx
+++ src/components/cards/NameCard.tsx
@@ -4,8 +4,6 @@ import {Avatar} from
 "@nextui-org/avatar";
 import { Button } from "@nextui-org/button";
 import Image from "next/image";
-import ToDoBtn from "../forms/toDoBtn";
-import HomeToDoBtn from "../forms/homeToDoBtn";
 
 interface Props {
     task: string;
@@ -16,7 +14,6 @@ export default function NameCard({task, date}: Props) {
     return (
         <div className="flex bg-white border-b-[1px] border-b-[#dee1ec] px-2 py-1 shadow-sm justify-between items-center "> 
             <div className="flex gap-2">
-                <HomeToDoBtn />
                 <p className="text-[11px] font-semibold">{task}</p>
             </div>
             <p className="text-[9px] font-bold text-[#152744]">{date}</p>
