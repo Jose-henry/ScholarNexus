82cb2183ee22a35c577f555a2808ecfa843c4297
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 16:15:26 +0100

    changes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import NameCard from "@/components/cards/NameCard";
 import { getUserByClerkId } from "@/lib/actions/user.action";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -15,7 +16,10 @@ export default async function Profile() {
     }
   return (
       <div className="w-full h-full pr-5 grid grid-cols-2 pb-3 gap-2 pt-2">
-        <div className="border border-black">
+        <div className="border border-black grid grid-rows-3">
+          <NameCard userName="foguibe" firstName="Fortune" middleName="Oluomachukwu" lastName="Oguibe" image={"/assets/profile-pic.jpg"}/>
+          <div className="border border-black"></div>
+          <div></div>
         </div>
         <div className="border border-black">
 
--- src/components/cards/NameCard.tsx
+++ src/components/cards/NameCard.tsx
@@ -16,9 +16,15 @@ interface Props {
 
 export default function NameCard({userName, firstName, lastName, middleName, image}: Props) {
     return (
-        <div> 
+        <div className="border border-black w-full"> 
             <Avatar showFallback isBordered radius="full" src="{image}" size="sm" className='cursor-pointer bg-slate-300 h-[25px] w-[25px]'>
             </Avatar>
+            <div>
+                <h2>First Name: {firstName}</h2>
+                <h2>Middle Name: {middleName}</h2>
+                <h2>Last Name: {lastName}</h2>
+                <h2>Username: {userName}</h2>
+            </div>
         </div>
     );
 }
