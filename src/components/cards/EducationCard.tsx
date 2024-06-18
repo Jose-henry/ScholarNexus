6f1a649ca5bd348c17f5e04d919c268d619ca639
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 19:24:57 +0100

    changes

--- src/components/cards/EducationCard.tsx
+++ src/components/cards/EducationCard.tsx
@@ -16,10 +16,10 @@ export default function EducationCard({programme, school, level}: Props) {
             <h2 className="font-bold text-[15px]">Education</h2>
             <div className="flex items-center gap-4">
                 <Image src="/assets/edu-pic.svg" width={150} height={150} alt="education"></Image>
-                <div>
-                    <h2 className="text-[#142d4c] font-bold text-[15px]">School: <span className="text-[14px] font-semibold">{school}</span></h2>
-                    <h2 className="text-[#142d4c] font-bold text-[15px]">Programme: <span className="text-[14px] font-semibold">{programme}</span></h2>
-                    <h2 className="text-[#142d4c] font-bold text-[15px]">Level: <span className="text-[14px] font-semibold">{level}</span></h2>
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-[#142d4c] font-extrabold text-[20px]">School: <span className="font-semibold text-black">{school}</span></h2>
+                    <h2 className="text-[#142d4c] font-extrabold text-[20px]">Programme: <span className="font-semibold text-black">{programme}</span></h2>
+                    <h2 className="text-[#142d4c] font-extrabold text-[20px]">Level: <span className="font-semibold text-black">{level}</span></h2>
                 </div>
             </div>
         </div>
