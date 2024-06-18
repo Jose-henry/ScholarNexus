ca29631b5380a61264022bcf1176856a2bf79184
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-18 05:24:18 +0100

    new

--- src/components/cards/notificationCard.tsx
+++ src/components/cards/notificationCard.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 
 export default function NotificationCard() {
     return (
-        <div className="w-[200px] h-[150px] rounded-sm shadow-md bg-gray-800 fixed right-2 top-13 z-10">
+        <div className="w-[200px] h-[150px] rounded-sm shadow-md bg-gray-800 fixed right-2 top-[50px] z-10 p-2">
+            <div>
+                <h2 className="text-white text-[13px] font-bold">Change theme:</h2>
+            </div>
+            <div>
+                <h2 className="text-white text-[13px] font-bold">Language:</h2>
+            </div>
         </div>
     );
 }
