4b2f57ef74eeb3e7ddd6088972b584e6d4659189
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-19 01:57:59 +0100

    changes

--- src/components/cards/serviceOffered.tsx
+++ src/components/cards/serviceOffered.tsx
@@ -13,12 +13,15 @@ interface Props {
 
 export default function ServiceOffered({jobTitle, jobNote, creationDate, deadline}: Props) {
     return (
-        <div className="rounded-sm bg-white border border-black shadow-sm p-2 w-full"> 
-            <h2>{jobTitle}</h2>
-            <p>{jobNote}</p>
-            <div className="flex justify-end gap-2 items-center">
-                <p>{creationDate}</p>
-                <p>{deadline}</p>
+        <div className="w-full flex gap-2 items-start"> 
+            <Image src="/assets/dot.svg" alt="" height={30} width={30} />
+            <div className="rounded-sm bg-white border-2 border-[#d3d6db] shadow-sm p-2">
+                <h2 className="font-bold text-[14px]">{jobTitle}</h2>
+                <p className="text-[13px]">{jobNote}</p>
+                <div className="flex justify-end gap-2 items-center mt-1">
+                    <p className="font-bold text-[12px]">posted: <span className="font-medium text-[11px]">{creationDate}</span></p>
+                    <p className="font-bold text-[12px]">deadline: <span className="font-medium text-[12px]">{deadline}</span></p>
+                </div>
             </div>
         </div>
     );
