c72d14a07000e4043cb7d5bd8df026d61b7bd301
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-19 03:03:59 +0100

    changes

--- src/components/cards/BioCard.tsx
+++ src/components/cards/BioCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export default function BioCard({bio}: Props) {
     return (
-        <div className="rounded-sm bg-white shadow-sm p-4 border-[2px] border-[#d3d6db]"> 
+        <div className="rounded-sm bg-white shadow-sm p-4 border-[1px] border-[#d3d6db]"> 
             <h2 className="font-bold text-[15px]">About</h2>
             <p className="pt-3">{bio}</p>
         </div>
--- src/components/cards/settingsCard.tsx
+++ src/components/cards/settingsCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function SettingsCard() {
     return (
-        <div className="w-[200px] h-[150px] bg-[#f8f8f8] border-2 border-[#606470] fixed right-2 top-[50px] z-10 px-3 py-2 flex flex-col gap-1 shadow-sm rounded-[3px]">
+        <div className="w-[200px] h-[150px] bg-[#f8f8f8] border-[1.5px] border-[#606470] fixed right-2 top-[50px] z-10 px-3 py-2 flex flex-col gap-1 shadow-sm rounded-[3px]">
             <div>
                 <h2 className="text-[11.5px] font-bold text-[#233142]">Change theme:</h2>
             </div>
